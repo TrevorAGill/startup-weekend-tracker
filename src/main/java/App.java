21d7b15751119b6dfe4ef53146bb025a62befc66
Trevor Gill <trevor.a.gill@gmail.com>
2017-08-11 12:42:17 -0700

    add main index page with event info and event listing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,36 @@
+import models.Event;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        //go to homepage
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Event> items= Event.getAllEvents();
+            model.put("items", items);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //go to event listing
+        get("/events", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Event> events = Event.getAllEvents();
+            model.put("events", events);
+            return new ModelAndView(model, "event-home.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+
+    }
 }

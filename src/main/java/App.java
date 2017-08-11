70744238cf78d07288bec365cf5bfe99b65d1cdb
Trevor Gill <trevor.a.gill@gmail.com>
2017-08-11 13:13:22 -0700

    add form to create new event and build out of event listing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class App {
@@ -28,6 +29,23 @@ public class App {
             model.put("events", events);
             return new ModelAndView(model, "event-home.hbs");
         }, new HandlebarsTemplateEngine());
+
+        //show new event form
+        get("/event/new", (request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return new ModelAndView(model, "event-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //add new event to event listing page
+        post("/event/new", (req,res)->{
+            Map<String, Object> model = new HashMap<>();
+            String name = req.queryParams("eventName");
+            String description = req.queryParams("eventDescription");
+            Event newEvent = new Event(name, description);
+            model.put("newEvent",newEvent);
+            res.redirect("/events");
+            return null;
+        });
 
 
 
